4f76e2476f8a18ed6620750d39f73d00a9635f63
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-05-28 09:36:26 +0300

    Create 04_priority_queues.java

---
+++ 04_priority_queues.java
@@ -0,0 +1,35 @@
+// Priority Queue:
+// - FIFO data structure 
+// - serves elements with the highest priorities first before elements with lower priority
+
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args){
+		
+		Queue<Double> queue1 = new PriorityQueue<>(Collections.reverseOrder()); // order: new PriorityQueue<>(); reverse order: new PriorityQueue<>(Collections.reverseOrder()); 
+
+		queue1.offer(3.0);
+		queue1.offer(2.5);
+		queue1.offer(4.0);
+		queue1.offer(1.5);
+		queue1.offer(2.0);
+		
+		while(!queue1.isEmpty()) {
+			System.out.println(queue1.poll());
+		}
+		
+		Queue<String> queue2 = new PriorityQueue<>();
+
+		queue2.offer("B");
+		queue2.offer("A");
+		queue2.offer("F");
+		queue2.offer("D");
+		queue2.offer("C");
+		
+		while(!queue2.isEmpty()) {
+			System.out.println(queue2.poll());
+		}
+		
+	}
+}
