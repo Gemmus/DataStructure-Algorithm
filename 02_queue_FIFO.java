fd00805d9f2f7025a79cc60870ad69b72b27df2b
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-05-28 09:25:32 +0300

    Create 02_queue_FIFO.java

---
+++ 02_queue_FIFO.java
@@ -0,0 +1,35 @@
+// Queue:
+// - FIFO(First-In First-Out) data structure ~ "a line of people": first-come, first-served
+// - a collection designed for holding elements prior processing
+// - linear data structure
+// - add = enqueue, offer()
+// - remove = dequeue, poll()
+
+// Use of stacks:
+// - keyboard buffer
+// - printer queue
+// - LinkedLists, PriorityQueues, Breadth-first search
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Main{
+	public static void main(String[] args){
+		
+		Queue<String> queue = new LinkedList<String>();
+		
+		queue.offer("Karen");
+		queue.offer("Steve");
+		queue.offer("Chad");
+		queue.offer("Harold");
+		System.out.println(queue.isEmpty());
+		System.out.println(queue.size());
+		
+		System.out.println(queue.peek());
+		System.out.print(queue.contains("Harold"));
+				
+		queue.poll();
+		System.out.println(queue);
+		
+	}
+}
