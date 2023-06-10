819a74ddb1c69c2b0e777e2522a4e55835b18cae
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-10 10:02:16 +0300

    Create 23_tree_data_structure_intro.java

---
+++ Set1/23_tree_data_structure_intro.java
@@ -0,0 +1,27 @@
+// Tree:
+// A non-linear data structure, where nodes are organized in a hierarchy.
+// Utilized: File explorer, Databases, DNS, HTML DOM
+
+// Root node (or parent):
+// On the top of the tree, has no in-coming edges, only out-going edges.
+
+// Branch nodes (or parent/child):
+// Between root node and leaf nodes, has both in-coming and out-going edges.
+
+// Leaf nodes (or child):
+// On the bottom of the tree, have no out-going edges, only in-coming edges.
+
+// Siblings: 
+// If they share the same parents.
+
+// Subtree:
+// Smaller tree held within a larger tree.
+
+// Size of the tree:
+// Number of nodes.
+
+// Depth:
+// Number of edges below the root node. (Root node level = 0)
+
+// Height
+// Number of edges above furthest leaf node. (Leaf node level = 0)
