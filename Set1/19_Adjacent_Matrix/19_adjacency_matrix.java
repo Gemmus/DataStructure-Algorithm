8dc7fb80337fa2ce40603deb57be44b864b631d6
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-10 08:26:21 +0300

    Create 19_adjacency_matrix.java

---
+++ Set1/19_Adjacent_Matrix/19_adjacency_matrix.java
@@ -0,0 +1,34 @@
+// Adjacency Matrix:
+// A 2D array to store 1's/0's to represent edges. (or can use booleans)
+//                        # of rows =        # of unique nodes
+//                        # of columns =     # of unique nodes
+
+// Runtime complexity to check an Edge: O(1).
+// Space complexity: O(v²)
+
+public class Main{
+	
+	public static void main(String[] args) {
+		
+		Graph graph = new Graph(5);
+		
+		graph.addNode(new Node('A'));
+		graph.addNode(new Node('B'));
+		graph.addNode(new Node('C'));
+		graph.addNode(new Node('D'));
+		graph.addNode(new Node('E'));
+		
+		graph.addEdge(0, 1); // A is connected to B
+		graph.addEdge(1, 2); // B is connected to C
+		graph.addEdge(2, 3); // C is connected to D
+		graph.addEdge(2, 4); // C is connected to E
+		graph.addEdge(4, 0); // E is connected to A
+		graph.addEdge(4, 2); // E is connected to C
+		
+		graph.print();
+		
+		System.out.println();
+		System.out.println(graph.checkEdge(0, 1)); // Edge AB?
+		System.out.println(graph.checkEdge(3, 2)); // Edge DC?
+	}
+}
