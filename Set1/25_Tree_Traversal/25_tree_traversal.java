aa8ebf456ecc19e888fd07071386eb3e5acf6d01
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-10 11:13:10 +0300

    Create 25_tree_traversal.java

---
+++ Set1/25_Tree_Traversal/25_tree_traversal.java
@@ -0,0 +1,10 @@
+// Types:
+
+// 1. In-order:
+// - left -> root -> right (DBEAFCH)
+
+// 2. Post-order:
+// - left -> right -> root (DEBFGCA)
+
+// 3. Pre-order: 
+// - root -> left -> right (ABDECFG)
