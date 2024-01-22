4932cc90d3c2e38064b704da67b6ba8802be8eb8
Gemmus <112064697+Gemmus@users.noreply.github.com>
2024-01-22 17:46:48 +0200

    Create main.cpp

---
+++ Set3/Exercise4/Exercise4b/Phase2/main.cpp
@@ -0,0 +1,95 @@
+/* Extra exercise 4b. (Converting infix notation to postfix notation, 0.25 bonus)
+Warning! This is quite demanding exercise.
+
+Phase 2
+
+Implement a better infix to postfix converter which allows also an infinite amount of
+parenthesis that control the order of evaluation. For example
+
+        Infix to postfix converter
+
+        Give an infix equation at one line
+        (((3+6+1)*(2-4))+7)
+        Equivalent postfix equation is 36+1+24-*7+
+*/
+
+#include <iostream>
+#include <ctype.h>
+#include <string.h>
+#include "stack.h"
+
+#define MAX_LENGTH 80
+
+using namespace std;
+
+int precedence(char ch);
+bool isOperand(char chr);
+void infixTopostfix(char *infix, char *postfix);
+
+int main() {
+
+    char infix[MAX_LENGTH], postfix[MAX_LENGTH];
+
+    cout << "Infix to postfix converter" << endl << endl;
+    cout << "Give an infix equation at one line" << endl;
+    cin.getline(infix, MAX_LENGTH);
+
+    infixTopostfix(infix, postfix);
+
+    cout << "Equivalent postfix equation is " << postfix << endl;
+
+    return 0;
+}
+
+int precedence(char chr){
+    switch(chr) {
+        case '-':
+        case '+':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        default:
+            return -1;
+    }
+}
+
+bool isOperand(char chr) {
+    return ('+' == chr || '-' == chr ||'*' == chr || '/' == chr);
+}
+
+void infixTopostfix(char *infix, char *postfix) {
+    Stack<char> stack;
+    int i = 0, j = 0;
+    char chr, aux;
+
+    stack.push('(');
+    strcat(infix, ")");
+
+    chr = infix[i];
+
+    while (chr != '\0') {
+        if (chr == '(') {
+            stack.push(chr);
+        } else if (isdigit(chr)) {
+            postfix[j++] = chr;
+        } else if (isOperand(chr)) {
+            stack.pop(aux);
+            while (isOperand(aux) && precedence(aux) >= precedence(chr)) {
+                postfix[j++] = aux;
+                stack.pop(aux);
+            }
+            stack.push(aux);
+            stack.push(chr);
+        } else if (chr == ')') {
+            stack.pop(aux);
+            while (aux != '(') {
+                postfix[j++] = aux;
+                stack.pop(aux);
+            }
+        }
+        chr = infix[++i];
+    }
+    
+    postfix[j] = '\0';
+}
