9965745427e1d73d7518cf328c289493e0aa068e
Gemmus <112064697+Gemmus@users.noreply.github.com>
2024-02-01 06:19:41 +0200

    Added extra print

--- Set3/Exercise7/Exercise7a/dynlist.h
+++ Set3/Exercise7/Exercise7a/dynlist.h
@@ -8,24 +8,24 @@ using namespace std;
 template <class T>
 class LinkedList {
 private:
-	// definition of the list node class
-	class Node {
-	public:
-		T     _value;	// data
-		Node *_pNext;	// pointer to the next node
-		Node() : _pNext(NULL) {}
-		Node(T val) : _value(val), _pNext(NULL) {}
-		Node(T val, Node* next) : _value(val), _pNext(next) {}
+    // definition of the list node class
+    class Node {
+    public:
+        T     _value;	// data
+        Node *_pNext;	// pointer to the next node
+        Node() : _pNext(NULL) {}
+        Node(T val) : _value(val), _pNext(NULL) {}
+        Node(T val, Node* next) : _value(val), _pNext(next) {}
 
-	};
+    };
 
-	Node *first;
+    Node *first;
 
 public:
-	LinkedList();
-	~LinkedList();
-	LinkedList<T>& insert_to_end(T data);
-	void print(std::ostream &out) const;
+    LinkedList();
+    ~LinkedList();
+    LinkedList<T>& insert_to_end(T data);
+    void print(std::ostream &out) const;
     void delete_first();
     void delete_last();
     bool find_pos(T item, int &pos);
@@ -43,10 +43,10 @@ LinkedList<T>::~LinkedList() {
 
     aux1 = first;
     while (aux1 != NULL) {
-	    aux2 = aux1->_pNext;
-	    delete aux1;
-	    printf("\nDeleted."); //for testing purposes
-	    aux1 = aux2;
+        aux2 = aux1->_pNext;
+        delete aux1;
+        printf("\nDeleted."); //for testing purposes
+        aux1 = aux2;
     }
 }
 
@@ -64,7 +64,7 @@ LinkedList<T>& LinkedList<T>::insert_to_end(T data) {
         what->_pNext = newnode;
         cout << "New node inserted to end which value is: " << newnode->_value << endl;
     }
-	return *this;
+    return *this;
 }
 
 template <class T>
@@ -74,16 +74,16 @@ void LinkedList<T>::print(std::ostream &out) const {
     out << "\nList:";
     what = first;
     while (what != NULL) {
-	    out << " " << what->_value << " ";
-	    what = what->_pNext;
+        out << " " << what->_value << " ";
+        what = what->_pNext;
     }
-	out << endl;
+    out << endl;
 }
 
 template <class T>
 std::ostream &operator<<(std::ostream &out,  LinkedList<T> const &data) {
-	data.print(out);
-	return out;
+    data.print(out);
+    return out;
 }
 
 template <class T>
@@ -117,6 +117,7 @@ void LinkedList<T>::delete_last() {
         newLast->_pNext->_value = '0';
         delete newLast->_pNext;
         newLast->_pNext = NULL;
+        cout << "Last node of the linked list was deleted." << endl;
     }
 }
 
